2523bc0aea6d26e53bb35561c146645c361a7509
jason2020 <jtay20@punahou.edu>
2016-06-29 14:58:35 -1000

    Completed the assignment

--- js/index.js
+++ js/index.js
@@ -33,11 +33,75 @@ window.onload = function () {
     if (req.readyState === XMLHttpRequest.DONE) {
       var data = JSON.parse(req.response)
       window.reddit_feed = data;
+      startProcess();
     }
   }
   var time = document.querySelectorAll('.time')
   for (var i = 0; i < time.length; i++) {
     var unixTime = time[i].innerHTML;
     time[i].innerHTML = moment.unix(unixTime);
+  }
+
+}
+
+function startProcess () {
+  var info = window.reddit_feed.data
+  var length = info.length;
+  // Code goes here:
+  for (var i = 0; i < length; i++) {
+    var obj = info[i];
+    console.log(obj);
+    var articleTitle = obj.title;
+    var articleThumbnail = obj.thumbnail;
+    var articleAuthor = obj.author;
+    var articleBody = obj.body;
+    var articleTime = obj.created;
+
+
+    var container = document.createElement('div');
+    var post = document.createElement('div');
+    var title = document.createElement('div');
+    var link = document.createElement('a');
+    var thumbnail = document.createElement('div');
+    var image = document.createElement('img');
+    var author = document.createElement('div');
+    var text = document.createElement('div');
+    var time = document.createElement('div');
+    
+    title.innerHTML = articleTitle;
+    title.className += "title";
+
+    thumbnail.appendChild(image);
+    link.appendChild(thumbnail);
+    link.appendChild(author);
+    link.appendChild(title);
+    link.appendChild(text);
+    link.appendChild(time);
+    post.appendChild(link);
+    container.appendChild(post);
+
+    post.className += "post";
+    link.href = "javascript:void(0)";
+    thumbnail.className += "thumbnail";
+    image.src = articleThumbnail;
+    image.alt = "thumbnail";
+    author.className += "author";
+    text.className += "body";
+    time.className += "time";
+
+    author.innerHTML = articleAuthor;
+    text.innerHTML = articleBody;
+    time.innerHTML = articleTime;
+
+    container.style.border = "1px solid black";
+
+    container.className += "col-sm-12 col-md-4";
+
+    document.getElementById("main").appendChild(container);
+
+    document.body.style.backgroundColor = "#bfff80";
+
+    // Styling
+
   }
 }
